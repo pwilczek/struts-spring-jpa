0abde4ba5ba2d7e4eb3f54a6a47d9a943993a6c5
Piotr Wilczek <piotr.wilczek@wilu.name>
2015-01-31 15:52:23 +0100

    Added spring integration.

---
+++ src/main/java/name/wilu/services/Greeter.java
@@ -0,0 +1,10 @@
+package name.wilu.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Greeter {
+    public String sayHello() {
+        return "Hello!~";
+    }
+}
