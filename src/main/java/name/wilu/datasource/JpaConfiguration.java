dcff46f92f1f1029d86e23097c72dae3c415c4f5
Piotr Wilczek <piotr.wilczek@wilu.name>
2015-02-01 11:17:12 +0100

    Added transaction support.

--- src/main/java/name/wilu/datasource/JpaConfiguration.java
+++ src/main/java/name/wilu/datasource/JpaConfiguration.java
@@ -2,14 +2,18 @@ package name.wilu.datasource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 public class JpaConfiguration {
 
     @Bean
@@ -29,5 +33,12 @@ public class JpaConfiguration {
         result.setJpaVendorAdapter(vendor);
         result.setPackagesToScan("name.wilu");
         return result;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean factory) {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(factory.getObject());
+        return manager;
     }
 }
